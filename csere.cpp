df9c03a8149c89c5cdf2f3192dea06b7788ae6c3
NincsJobbOtletem <55430954+NincsJobbOtletem@users.noreply.github.com>
2019-09-22 10:01:59 +0200

    Update csere.cpp

--- csere.cpp
+++ csere.cpp
@@ -9,3 +9,16 @@ int main()
     x = x / y;
     cout << "csere: x=" << x << ", y=" << y;
 }
+
+#include "std_lib_facilities.h"
+
+int main()
+{
+    int x = 8, y = 3;
+
+    x = x - y;
+    y = x + y;
+    x = y - x;
+    cout << "csere: x=" << x << ", y=" << y;
+}
+

ce27623d4587251ca43130e5996d90dddc6a7316
NincsJobbOtletem <55430954+NincsJobbOtletem@users.noreply.github.com>
2019-09-22 14:44:24 +0200

    Create shifteles.cpp

---
+++ shifteles.cpp
@@ -0,0 +1,12 @@
+#include "std_lib_facilities.h"
+int main()
+{
+  int i=5,db=0;
+
+  while (i!=0){
+      i=i<<1;
+      db++;
+      cout<<i<<"\n";
+      }
+  cout<<"Cserek szama: "<<db<<"\n";
+}

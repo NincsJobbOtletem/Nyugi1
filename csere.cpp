42dcb329e9708aa09c8d0a7490785f1807775f6a
NincsJobbOtletem <55430954+NincsJobbOtletem@users.noreply.github.com>
2019-09-22 10:06:57 +0200

    Update csere.cpp

--- csere.cpp
+++ csere.cpp
@@ -22,3 +22,13 @@ int main()
     cout << "csere: x=" << x << ", y=" << y;
 }
 
+#include "std_lib_facilities.h"
+
+int main()
+{
+    int x = 8, y = 3;
+        x ^= y;
+        y ^= x;
+        x ^= y;
+    cout << "csere xor al: x=" << x << ", y=" << y;
+}

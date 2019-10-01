ed96e9a998266d92fd9bbd619bd2996797e17a18
NincsJobbOtletem <55430954+NincsJobbOtletem@users.noreply.github.com>
2019-10-01 14:58:58 +0200

    Update drill5.cpp

--- drill5.cpp
+++ drill5.cpp
@@ -1,1 +1,33 @@
+#include "std_lib_facilities.h"
 
+int main()
+{
+
+cout << "Success!\n";
+cout << "Success!\n
+cout << "Success! \n";
+cout << "success! \n" << endl;
+int res = 7; vector<int> v(10); v[51]= res; cout << "Success!\n";
+vector<int> v(10); v{5} = 7; if (v(5) ==7) cout << "Success!\n";
+if (1==1) {cout << "Success!\n "; else cout« "Fail!\n";}
+bool c = false; if (c) cout << "Success!\n"; else cout « "Fail!\n ";
+string s = "ape"; bool c = ("fool "<s); if (e) coul« "Success!\n ";
+string s = "ape"; if (5=="fool ") coul« "Success!\n";
+string s = "ape"; if (s=="fool ") coul < "Success!\n";
+string s = "ape"; if (s+"fool ") coul < "Success!\n";
+vector<char> v(5); for (int i=O; k<v.size(); ++i); cout << "Success!\n";
+vector<char> v(5); for (int i=O; k=v.size(); ++i); cout << "Success!\n";
+string s = "Success!\n "; for (int i::O; k6; ++i); cout << "Success!\n";
+if (true) {cout << "Success!\n"; else cout << "Fail!\n";}
+int x = 2000; char c = x; if (c==2000) cout << "Success!\n";
+string s = "Success!\n"; for (int i::O; k 10; ++i) cout << "Success!\n";
+vector<char> v(5); for (int i=O; k=v.size(); ++i) ; cout << "Success!\n";
+int i = 0; int j = 9; while (kl0) ++j; if (j<i) cout << "Success!\n";
+int x = 2; double d = 51(x-2); if (d==2·x+O.5) cout << "Success!\n";
+string s = "Success!\n" ; for (int i::O; i<=10; ++i) cout << "Success!\n"];
+int i=O; while (i<10) ++j; if (j<i) cout « "Success!\n";
+int x = 4; double d = 5.1*(x-2); if (d!=2*x+O.5) cout << "Success!\n";
+cout << "Success!\n";
+  
+	return 0;
+}
